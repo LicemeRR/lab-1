9899d10e6c4615456b405426ac5787d0109be742
LicemeR <80973735+LicemeRR@users.noreply.github.com>
2022-09-25 18:08:08 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@ int main()
 {	
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	char name[] = " ";
+        string name;
 	cout << "Ââåä³òü âàøå ¿ìÿ" << endl;
 	cin >> name;
 	cout << "Ïðèâ³ò"<<" "<< name << "!";
